c935549a2c10bdb9e894cd9100fdffc462e95ba1
zanacos <zana.cosic@stud.fra-uas.de>
2023-11-28 18:32:35 +0100

    test

---
+++ zwei/src/main/java/edu/fra/uas/hausaufgabe/zwei/ZweiApplication.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.hausaufgabe.zwei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZweiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZweiApplication.class, args);
+	}
+
+}
---
+++ zwei/src/test/java/edu/fra/uas/hausaufgabe/zwei/ZweiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.fra.uas.hausaufgabe.zwei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZweiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
